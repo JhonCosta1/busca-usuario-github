0370419bbde871fa6b630e2e990509ece64669b1
Jhon Costa <jhon.costa.dev@gmail.com>
2023-01-30 17:47:51 -0300

    Criação da lógica
    Criada variáveis para setar tags;
    Criada a requisição AJAX;
    Criada a lógica JSON para formatar dados para Array;

---
+++ logica.js
@@ -0,0 +1,62 @@
+let btn = document.querySelector('#btn');
+let input = document.querySelector('input[name=github_user]');
+let div = document.querySelector('#app');
+
+
+btn.onclick = function(){
+    //Limpar o conteúdo da div
+    div.innerHTML.html = '';
+
+    //Instanciando objeto AJAX
+
+    let ajax = new XMLHttpRequest();
+
+    //Abrir uma conexão / ajax.readyState -> 0 antes da conexão ser aberta
+    ajax.open('GET', `https://api.github.com/users/${github_user}`);
+
+    //Enviar a requisição / ajax.readyState -> 1 após abrir a conexão
+    ajax.send(null);
+
+    //Criar elemento span
+    ajax.onreadystatechange = function(){
+
+        //Criar elemento span
+        let spanNone = document.createElement('span');
+
+        //Criar a variável none
+        let txtNome = '';
+
+        /*  
+            ajax.readyState -> 0 antes da conexão ser aberta
+            ajax.readyState -> 1 após abrir a conexão
+            ajax.readyState -> 2 headers(cabeçalhos) foram recebidos
+            ajax.readyState -> 3 carregando corpo da requisição /corpo dos dados
+            ajax.readyState -> 4 O conteudo está pronto pra uso
+        */
+        if(ajax.readyState === 4){
+
+            if(ajax.status = 200){
+
+                //transforma JSON para array
+                usuario = JSON.parse(ajax.responseText);
+
+
+                // se usuário possui nome
+                if(usuario['name'] !== null){
+                    txtNome = document.createTextNode(usuario['name']);
+
+                    let img = document.createElement('img');
+                    img.setAttribute('src', usuario['avatar_url']);
+                    img.setAttribute('alt', usuario['name']);
+                    img.setAttribute('width', '45px');
+                    img.setAttribute('height', '45px');
+
+                    div.appendChild(img);
+
+                }
+            }
+        }
+    }
+
+    
+}
